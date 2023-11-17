6c9485daf9b0d466b4db5a7eb36c2cdb665e4e0b
Parker <parkied@gmail.com>
2023-11-17 13:40:23 -0700

    Initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('assets');
+
+    eleventyConfig.addFilter('head', (array, n) => {
+        if (!Array.isArray(array) || array.length === 0) {
+            return [];
+        }
+        if (n < 0) {
+            return array.slice(n);
+        }
+
+        return array.slice(0, n);
+    });
+};

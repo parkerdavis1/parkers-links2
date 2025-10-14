cf1db3a5bb41e7dc5c62aee162eee0c5e0216f2c
Parker <parkied@gmail.com>
2025-10-13 17:08:57 -0700

    autoformat

--- _data/parkerdavis.js
+++ _data/parkerdavis.js
@@ -1,12 +1,11 @@
-const EleventyFetch = require("@11ty/eleventy-fetch");
+const EleventyFetch = require('@11ty/eleventy-fetch');
 
-module.exports = async function() {
-  let url = "https://parkerdavis.dev/feed/feed.json";
+module.exports = async function () {
+    let url = 'https://parkerdavis.dev/feed/feed.json';
 
-  /* This returns a promise */
-  return EleventyFetch(url, {
-    duration: "1d", // save for 1 day
-    type: "json"    // we’ll parse JSON for you
-  });
+    /* This returns a promise */
+    return EleventyFetch(url, {
+        duration: '1d', // save for 1 day
+        type: 'json', // we’ll parse JSON for you
+    });
 };
-
